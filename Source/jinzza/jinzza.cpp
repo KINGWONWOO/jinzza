cb8b41e58bcd665c7e36eaafa790b6a914b9bcb1
kingwonwoo <king_wonwoo@naver.com>
2026-08-23 18:20:47 +0900

    Initial commit: UE5.8 project scaffold + game design doc
    - Third-person/multiplayer template base (Source/, Config/, Content/)
      with Horror and Shooter variants, Steam OnlineSubsystem enabled.
    - docs/gamestart.docx: design doc for social deduction party game
      (working title "진짜를 찾아라").
    - docs/workLog.txt: dated work log for tracking progress across sessions.
    - .gitignore / .gitattributes: Unreal-standard ignores + Git LFS tracking
      for .uasset/.umap and other binary assets.

---
+++ Source/jinzza/jinzza.cpp
@@ -0,0 +1,8 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "jinzza.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, jinzza, "jinzza" );
+
+DEFINE_LOG_CATEGORY(Logjinzza)
---
+++ Source/jinzza/jinzzaGameMode.cpp
@@ -0,0 +1,8 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "jinzzaGameMode.h"
+
+AjinzzaGameMode::AjinzzaGameMode()
+{
+	// stub
+}
